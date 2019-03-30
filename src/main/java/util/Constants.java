c34b1e92276a9cae8f0295c9f75f6801aad1bfe4
spring <spring@qbserver.cn>
2019-03-30 21:19:51 +0800

    init

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * 常量
+ * 
+ * @author Administrator
+ *
+ */
+public class Constants {
+	// 外网
+	public static String BASE_URL_EXTERNAL = "http://60.218.184.234:8091";
+	// 内网查询图书定位
+	public static String BASE_URL_INTERNAL_QUERY_LOCATION = "http://10.0.15.12/TSDW/GotoFlash.aspx?szBarCode=";
+}
